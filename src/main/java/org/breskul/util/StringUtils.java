bdb4aae7b51ef1442668e25ae2b4661729cbc834
gleb <glebgomenyk@gmail.com>
2022-09-02 16:42:10 +0300

    SessionFactory

--- src/main/java/org/breskul/util/StringUtils.java
+++ src/main/java/org/breskul/util/StringUtils.java
@@ -1,7 +1,12 @@
 package org.breskul.util;
 
 public class StringUtils {
+    private final static String PARAM_FOR_REPLACE = "$1_$2";
+    private final static String FIRST_RGX = "([A-Z]+)([A-Z][a-z])";
+    private final static String SECOND_RGX = "([a-z])([A-Z])";
     public static String camelToSnake(String camelString) {
-        return camelString.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2").replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
+
+
+        return camelString.replaceAll(FIRST_RGX, PARAM_FOR_REPLACE).replaceAll(SECOND_RGX, PARAM_FOR_REPLACE).toLowerCase();
     }
 }
